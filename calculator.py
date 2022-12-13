ca96872fccdf4c1b4c5982ee1721f40ad4c2c1b1
KonstantinVasiliev356156 <356156@edu.itmo.ru>
2022-12-13 21:26:49 +0300

    File changed

---
+++ calculator.py
@@ -0,0 +1,15 @@
+operation = input()
+operation = list(operation.split())
+a = int(operation[0])
+b = int(operation[2])
+operator = operation[1]
+answer = "wrong input";
+if(operator == '+'):
+    answer = a + b
+elif (operator == '-'):
+    answer = a - b
+elif (operator == '*'):
+    answer = a * b
+elif (operator == '/'):
+    answer = a / b
+print(answer)
