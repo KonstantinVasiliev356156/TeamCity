7bde4b0b5a9a735d3f7e25dfa9c130033a561441
KonstantinVasiliev356156 <113301746+KonstantinVasiliev356156@users.noreply.github.com>
2022-12-14 10:16:25 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,7 @@ class Tests:
         assert output == 4
     def test_multiply(self):
         output = calculator.calculate("8 * 4")
-        assert output == 20
+        assert output == 32
     def test_divide(self):
         output = calculator.calculate("8 / 4")
         assert output == 2
